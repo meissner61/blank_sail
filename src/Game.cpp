f6cc763275d3a1282b46061eb50397b03abbc6e3
Vlad <vlad.lopatinsky@gmail.com>
2023-10-24 17:19:19 -0700

    Cleaned up the PrintWindowEvents() function

--- src/Game.cpp
+++ src/Game.cpp
@@ -199,6 +199,7 @@ void Game::PrintWindowEvents(const SDL_Event *event)
 
     //see more here:
     //https://wiki.libsdl.org/SDL2/SDL_WindowEvent
+    //https://wiki.libsdl.org/SDL2/SDL_WindowEventID
 
     if(event->type == SDL_WINDOWEVENT)
     {
@@ -214,57 +215,75 @@ void Game::PrintWindowEvents(const SDL_Event *event)
                 SDL_Log("Window %d hidden", event->window.windowID);
                 break;
             }
-        case SDL_WINDOWEVENT_EXPOSED:
-        {
-            SDL_Log("Window %d exposed", event->window.windowID);
-            break;
-        }
-        case SDL_WINDOWEVENT_MOVED:
-        {
-            SDL_Log("Window %d moved to %d,%d",
-                    event->window.windowID, event->window.data1,
-                    event->window.data2);
-            break;
-        }
-        case SDL_WINDOWEVENT_RESIZED:
-        {
-            SDL_Log("Window %d resized to %dx%d",
-                    event->window.windowID, event->window.data1,
-                    event->window.data2);
-            break;
-        }
-        case SDL_WINDOWEVENT_SIZE_CHANGED:
-            SDL_Log("Window %d size changed to %dx%d",
-                    event->window.windowID, event->window.data1,
-                    event->window.data2);
-            break;
-        case SDL_WINDOWEVENT_MINIMIZED:
-            SDL_Log("Window %d minimized", event->window.windowID);
-            break;
-        case SDL_WINDOWEVENT_MAXIMIZED:
-            SDL_Log("Window %d maximized", event->window.windowID);
-            break;
-        case SDL_WINDOWEVENT_RESTORED:
-            SDL_Log("Window %d restored", event->window.windowID);
-            break;
-        case SDL_WINDOWEVENT_ENTER:
-            SDL_Log("Mouse entered window %d",
-                    event->window.windowID);
-            break;
-        case SDL_WINDOWEVENT_LEAVE:
-            SDL_Log("Mouse left window %d", event->window.windowID);
-            break;
-        case SDL_WINDOWEVENT_FOCUS_GAINED:
-            SDL_Log("Window %d gained keyboard focus",
-                    event->window.windowID);
-            break;
-        case SDL_WINDOWEVENT_FOCUS_LOST:
-            SDL_Log("Window %d lost keyboard focus",
-                    event->window.windowID);
-            break;
-        case SDL_WINDOWEVENT_CLOSE:
-            SDL_Log("Window %d closed", event->window.windowID);
-            break;
+            case SDL_WINDOWEVENT_EXPOSED:
+            {
+                SDL_Log("Window %d exposed", event->window.windowID);
+                break;
+            }
+            case SDL_WINDOWEVENT_MOVED:
+            {
+                SDL_Log("Window %d moved to %d,%d",
+                        event->window.windowID, event->window.data1,
+                        event->window.data2);
+                break;
+            }
+            case SDL_WINDOWEVENT_RESIZED:
+            {
+                SDL_Log("Window %d resized to %dx%d",
+                        event->window.windowID, event->window.data1,
+                        event->window.data2);
+                break;
+            }
+            case SDL_WINDOWEVENT_SIZE_CHANGED:
+            {
+                SDL_Log("Window %d size changed to %dx%d",
+                        event->window.windowID, event->window.data1,
+                        event->window.data2);
+                break;
+            }
+            case SDL_WINDOWEVENT_MINIMIZED:
+            {
+                SDL_Log("Window %d minimized", event->window.windowID);
+                break;
+            }
+            case SDL_WINDOWEVENT_MAXIMIZED:
+            {
+                SDL_Log("Window %d maximized", event->window.windowID);
+                break;
+            }
+            case SDL_WINDOWEVENT_RESTORED:
+            {
+                SDL_Log("Window %d restored", event->window.windowID);
+                break;
+            }
+            case SDL_WINDOWEVENT_ENTER:
+            {
+                SDL_Log("Mouse entered window %d",
+                        event->window.windowID);
+                break;
+            }
+            case SDL_WINDOWEVENT_LEAVE:
+            {
+                SDL_Log("Mouse left window %d", event->window.windowID);
+                break;
+            }
+            case SDL_WINDOWEVENT_FOCUS_GAINED:
+            {
+                SDL_Log("Window %d gained keyboard focus",
+                        event->window.windowID);
+                break;
+            }
+            case SDL_WINDOWEVENT_FOCUS_LOST:
+            {
+                SDL_Log("Window %d lost keyboard focus",
+                        event->window.windowID);
+                break;
+            }
+            case SDL_WINDOWEVENT_CLOSE:
+            {
+                SDL_Log("Window %d closed", event->window.windowID);
+                break;
+            }
 
         }
     }
