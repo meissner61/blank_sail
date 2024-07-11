f2e2f5724160bddf648f73b11f3dfdfc55b4ac4a
Vlad <vlad.lopatinsky@gmail.com>
2024-07-11 16:04:51 -0700

    Light test, not a barebone APP

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,69 @@
+#include <SDL.h>
+#include "TextureManager.h"
+//#include <SDL_image.h>
 
-#include <iostream>
-#include <algorithm>
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);  // Initialize SDL2
+    //IMG_Init(IMG_INIT_PNG);    // Initialize SDL2_image
 
-#include "Game.h"
-#include "Util.h"
-#include "extern/glm/vec2.hpp"
-#include "Random.h"
+    SDL_Window* window = SDL_CreateWindow("SDL Lighting Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-#ifdef _WIN32
-#define NOMINMAX //for <limits> macro inside windows.h conflicts with this header
-#include <windows.h>
-#endif
+    sail::TextureManager::GetInstance().Init(renderer);
 
-#include <string>
-
-#include <limits>
-#include <iomanip>
-
-
-int main(int argc, char* argv[])
-{
-    (void)argc;
-    (void)argv;
-
-    #ifdef _WIN32 //set console position to top left corner
-        HWND consoleWindow = GetConsoleWindow();
-        SetWindowPos(consoleWindow, 0, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
-    #endif
-
-    if constexpr(sizeof(void*) == 8) //constexpr == solution for warning C4127: conditional expression is constant c++17
-    {
-        std::cout << "64-bit architecture..\n";
-    }
-    else if constexpr(sizeof(void*) == 4)
-    {
-        std::cout << "32-bit architecture..\n";
-    }
+    // Load images
+    sail::TextureManager::GetInstance().LoadTexture("../data/terrain.png");
+    // SDL_Surface* terrainSurface = IMG_Load("terrain.png");
+    // SDL_Texture* terrainTexture = SDL_CreateTextureFromSurface(renderer, terrainSurface);
+    // SDL_FreeSurface(terrainSurface);
 
 
-    sail::Util::FindRootEnginePath();
-
-    sail::Util::DisaplyProjectRootPath();
-
-
-    if(sail::Util::root_path.empty()) //TODO: FIX This, not working properly. add a root_path_name for a stemmed version of root path and check against engine_name
-    {
-        std::cout << "WARNING! no project path set. Possible errors upon loading data!\n";
-        std::cout << "See Util.h -> root_path\n";
-    }
+    sail::TextureManager::GetInstance().LoadTexture("../data/light.png");
+    // SDL_Surface* lightSurface = IMG_Load("light.png");  // Light.png should be a radial gradient
+    // SDL_Texture* lightTexture = SDL_CreateTextureFromSurface(renderer, lightSurface);
+    // SDL_FreeSurface(lightSurface);
 
 
-    
-    
+    SDL_Texture* lightTexture = sail::TextureManager::GetInstance().GetTexture("light");
+
+    SDL_SetTextureBlendMode(lightTexture, SDL_BLENDMODE_ADD);  // Set blend mode to add to simulate lighting
 
     
 
-    Game game;
+    bool running = true;
+    SDL_Event event;
+    int mouseX, mouseY;
 
-    game.Run();
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_GetMouseState(&mouseX, &mouseY);  // Get mouse position
+
+        // Clear screen
+        SDL_RenderClear(renderer);
+
+        // Render terrain
+        SDL_RenderCopy(renderer, sail::TextureManager::GetInstance().GetTexture("terrain"), NULL, NULL);
+
+        // Render light at mouse position
+        SDL_Rect destRect = { mouseX - 128, mouseY - 128, 256, 256 };  // Adjust size and position as needed
+        SDL_RenderCopy(renderer, lightTexture, NULL, &destRect);
+
+        // Update screen
+        SDL_RenderPresent(renderer);
+    }
+
+    // Clean up
+    //SDL_DestroyTexture(terrainTexture);
+    //SDL_DestroyTexture(lightTexture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    //IMG_Quit();
+    SDL_Quit();
 
     return 0;
 }
