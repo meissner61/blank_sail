2c41ef75b2efdd8461363fb393922d4e59b70c8b
Vlad <vlad.lopatinsky@gmail.com>
2026-02-19 16:41:09 -0800

    Controller input tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,111 +1,144 @@
-
-#include <iostream>
-#include <algorithm>
-
-//#include "Game.h"
-#include "Util.h"
-#include "extern/glm/vec2.hpp"
-#include "Random.h"
-
-#include "SDL.h"
-
-#ifdef _WIN32
-#define NOMINMAX //for <limits> macro inside windows.h conflicts with this header
-#include <windows.h>
-#endif
-
+// controller_info.cpp
+#include <SDL.h>
+#include <cstdio>
 #include <string>
 
-#include <limits>
-#include <iomanip>
+// Helper: get a short ID string like "045E:028E" from vendor/product
+std::string VIDPID(SDL_GameController *gc)
+{
+    Uint16 vid = SDL_GameControllerGetVendor(gc);
+    Uint16 pid = SDL_GameControllerGetProduct(gc);
+    char buf[10];
+    std::snprintf(buf, sizeof(buf), "%04X:%04X", vid, pid);
+    return buf;
+}
+
+void listExistingControllers()
+{
+    int n = SDL_NumJoysticks();
+    for (int i = 0; i < n; ++i)
+    {
+        if (!SDL_IsGameController(i))
+            continue;                       // Skip unknown devices
+
+        SDL_GameController *gc = SDL_GameControllerOpen(i);
+        if (!gc)
+        {
+            SDL_Log("Couldnt open controller %d: %s", i, SDL_GetError());
+            continue;
+        }
+
+        SDL_JoystickID id = SDL_JoystickInstanceID(SDL_GameControllerGetJoystick(gc));
+        SDL_Log("Found [%d] %s (%s)",
+                id,
+                SDL_GameControllerName(gc),
+                VIDPID(gc).c_str());
+
+        SDL_GameControllerClose(gc);        // Just probing – close it again
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER) != 0)
+    {
+        SDL_Log("SDL_Init failed: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_ControllerButtonEvent controller;
+
+    if(controller.button == SDL_CONTROLLER_BUTTON_X)
+    {
+        puts("X!!!");
+    }
 
 
-// Screen dimensions
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+    SDL_Log("Press ESC to quit.");
+    listExistingControllers();
 
-// Function to draw a filled circle (Bresenham's Midpoint Circle Algorithm)
-void DrawFilledCircle(SDL_Renderer* renderer, int cx, int cy, int radius) {
-    for (int w = 0; w < radius * 2; w++) {
-        for (int h = 0; h < radius * 2; h++) {
-            int dx = radius - w; // Horizontal offset
-            int dy = radius - h; // Vertical offset
-            if ((dx * dx + dy * dy) <= (radius * radius)) {
-                SDL_RenderDrawPoint(renderer, cx + dx, cy + dy);
+    bool running = true;
+    while (running)
+    {
+        SDL_Event e;
+        while (SDL_PollEvent(&e))
+        {
+            if(e.type == SDL_KEYDOWN)
+            {
+                switch (e.key.keysym.sym)
+                {
+                    case SDLK_i:
+                    {
+                        puts("i key pressed");
+                    }
+                }
+            }
+
+            switch (e.type)
+            {
+                case SDL_QUIT:
+                    running = false;
+                    break;
+
+                case SDL_KEYDOWN:
+                {
+                    if (e.key.keysym.sym == SDLK_k)
+                    {
+                        puts("k key pressed");
+                    }
+                        
+                    break;
+                }
+                
+
+                case SDL_CONTROLLER_BUTTON_X :
+                {
+                    if(e.cbutton.button == SDL_CONTROLLER_BUTTON_X)
+                    {
+                        puts("Controller right axxis");
+                    }
+                    float rightx = 0;
+                    
+                    break;
+                }
+
+                case SDL_JOYAXISMOTION:
+                {
+                    puts("Joy axis motion");
+                }
+
+                // A new device appeared (plug‑in)
+                case SDL_CONTROLLERDEVICEADDED:
+                {
+                    int devIndex = e.cdevice.which;            // index in current list
+                    SDL_GameController *gc = SDL_GameControllerOpen(devIndex);
+                    if (!gc)
+                    {
+                        SDL_Log("Couldn’t open new controller: %s", SDL_GetError());
+                        break;
+                    }
+                    SDL_JoystickID id = SDL_JoystickInstanceID(SDL_GameControllerGetJoystick(gc));
+                    SDL_Log(">> Attached [%d] %s (%s)",
+                            id,
+                            SDL_GameControllerName(gc),
+                            VIDPID(gc).c_str());
+                    // Keep it open so we can read later, or close immediately:
+                    SDL_GameControllerClose(gc);
+                    break;
+                }
+
+                // Device removed (unplug)
+                case SDL_CONTROLLERDEVICEREMOVED:
+                {
+                    SDL_JoystickID id = e.cdevice.which;
+                    SDL_Log("<< Removed  [%d]", id);
+                    break;
+                }
             }
         }
-    }
-}
-
-bool InitSDL(SDL_Window** window, SDL_Renderer** renderer) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-        return false;
-    }
-    *window = SDL_CreateWindow("Screen Wrapping", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-    if (*window == nullptr) {
-        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        return false;
-    }
-    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
-    if (*renderer == nullptr) {
-        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        return false;
-    }
-    SDL_SetRenderDrawColor(*renderer, 255, 255, 255, 255); // Set background color
-    return true;
-}
-
-
-
-int main(int argc, char* argv[])
-{
-    (void)argc;
-    (void)argv;
-
-    #ifdef _WIN32 //set console position to top left corner
-        HWND consoleWindow = GetConsoleWindow();
-        SetWindowPos(consoleWindow, 0, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
-    #endif
-
-    if constexpr(sizeof(void*) == 8) //constexpr == solution for warning C4127: conditional expression is constant c++17
-    {
-        std::cout << "64-bit architecture..\n";
-    }
-    else if constexpr(sizeof(void*) == 4)
-    {
-        std::cout << "32-bit architecture..\n";
-    }
-    //__debugbreak();
-
-    sail::Util::FindRootEnginePath();
-
-    sail::Util::DisaplyProjectRootPath();
-
-
-    if(sail::Util::root_path.empty()) //TODO: FIX This, not working properly. add a root_path_name for a stemmed version of root path and check against engine_name
-    {
-        std::cout << "WARNING! no project path set. Possible errors upon loading data!\n";
-        std::cout << "See Util.h -> root_path\n";
+        SDL_Delay(10);   // Tiny sleep so we’re not 100 % CPU
     }
 
-
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
-
-    if (!InitSDL(&window, &renderer)) {
-        std::cerr << "Failed to initialize!" << std::endl;
-        return -1;
-    }
-
-    
-    std::getchar();
-
-    
-
-    // Game game;
-
-    // game.Run();
-
+    SDL_Quit();
     return 0;
 }
